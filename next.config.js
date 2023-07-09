8bf5c3cda089bf24d0c8f59a1754c9fad08b7ec3
Ananya Tomar <73813539+Ananyatomar205@users.noreply.github.com>
2023-07-09 18:29:35 +0530

    feat: setup cloudinary and multer

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
   env: {
     API_URL: "http://localhost:3000",
     DB_URI: "mongodb://localhost:27017/buyitnow",
+    CLOUD_NAME: "droarxty8",
+    CLOUDINARY_API_KEY: "477418877662747",
+    CLOUDINARY_API_SECRET: "Oqu7wE_Uo-Xfg9-vjruk-aSODIk",
     NEXTAUTH_SECRET: "ecommerceprojectAnanya",
   },
   images: {
