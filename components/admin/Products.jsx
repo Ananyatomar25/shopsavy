4f1bc620626de7b80175c14027ddcb7282c50894
Ananya Tomar <73813539+Ananyatomar205@users.noreply.github.com>
2023-07-19 20:23:40 +0530

    feat: update and delete products from admin dashboard

--- components/admin/Products.jsx
+++ components/admin/Products.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+"use client";
+
+import React, { useContext, useEffect } from "react";
 import Link from "next/link";
 import CustomPagination from "../layouts/CustomPagination";
+import ProductContext from "@/context/ProductContext";
+import { toast } from "react-toastify";
 
 const Products = ({ data }) => {
+  const { deleteProduct, error, clearErrors } = useContext(ProductContext);
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      clearErrors();
+    }
+  }, [error]);
+
+  const deleteHandler = (id) => {
+    deleteProduct(id);
+  };
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
       <h1 className="text-3xl my-5 ml-4 font-bold">
@@ -30,7 +46,7 @@ const Products = ({ data }) => {
             <tr className="bg-white">
               <td className="px-6 py-2">{product?.name}</td>
               <td className="px-6 py-2">{product?.stock}</td>
-              <td className="px-6 py-2">₹{product?.price}</td>
+              <td className="px-6 py-2">${product?.price}</td>
               <td className="px-6 py-2">
                 <div>
                   <Link
@@ -41,12 +57,15 @@ const Products = ({ data }) => {
                   </Link>
 
                   <Link
-                    href={`/admin/products`}
+                    href={`/admin/products/${product?._id}`}
                     className="px-2 py-2 inline-block text-yellow-600 bg-white shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 cursor-pointer mr-2"
                   >
                     <i className="fa fa-pencil" aria-hidden="true"></i>
                   </Link>
-                  <a className="px-2 py-2 inline-block text-red-600 bg-white shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 cursor-pointer">
+                  <a
+                    className="px-2 py-2 inline-block text-red-600 bg-white shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 cursor-pointer"
+                    onClick={() => deleteHandler(product?._id)}
+                  >
                     <i className="fa fa-trash" aria-hidden="true"></i>
                   </a>
                 </div>
